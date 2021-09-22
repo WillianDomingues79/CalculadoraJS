91f9cd94ffe198b544b3d016306a5e88bd271497
WillianDomingues79 <54421683+WillianDomingues79@users.noreply.github.com>
2021-09-22 13:22:09 -0300

    Inclusão dos cases dos botões e suas funções

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,7 @@
 class CalcController{
 
-    constructor(){
+    constructor(){//ATRIBUTOS
+        this._operation = [];
         this.locale = 'pt-BR'//Criado pois irá usar varias vezes
         this._displayCalcEl = document.querySelector("#display")
         this._dataEl = document.querySelector("#data")
@@ -24,6 +25,82 @@ class CalcController{
         });
     }
 
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+
+        switch(value){
+
+            case "ac":
+                this.clearAll();
+            break;
+
+            case "ce":
+                this.clearEntry();
+            break;
+
+            case "soma":
+                this.somar();
+            break;
+
+            case "subtracao":
+                this.somar();
+            break;
+
+            case "divisao":
+                this.somar();
+            break;
+
+            case "multiplicacao":
+                this.somar();
+            break;
+
+            case "porcento":
+                this.somar();
+            break;
+
+            case "igual":
+                this.somar();
+            break;
+
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                this.addOperation(parseInt(value));
+            //adiciona o value que recebeu do botão e converte para inteiro
+            //Precisa ser feito desta forma pois o nome dos botões é uma string 
+            break;
+
+            default:
+                this.setError();
+            break
+        }
+    }
+
     //Evento do clique dos botoes
     initButtonsEvents() {
         //Cria uma variavel para poder pegar todos os botoes pelo ID do DOM
@@ -32,7 +109,10 @@ class CalcController{
         //ForEach percorre o evento do click para quando ser clicado mostrar na tela já formatado
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal.replace("btn-",""));
+                //console.log(btn.className.baseVal.replace("btn-",""));
+                let textBtn = btn.className.baseVal.replace("btn-","");
+                this.execBtn(textBtn);
+
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
