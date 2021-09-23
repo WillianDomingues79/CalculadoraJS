88e3422def5cc9c70d0afd1e1e970aed86d6027a
WillianDomingues79 <54421683+WillianDomingues79@users.noreply.github.com>
2021-09-22 21:34:37 -0300

    Adicionado validadores para os operadores e metodos para somar apenas 2 numeros por operação

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -19,31 +19,112 @@ class CalcController{
        }, 1000)
     }
 
-    addEventListenerAll(element, events, fn){
+    //Função para receber as coisas e criar um array de Strings
+    //A string seria o "click drag" que tem logo ali abaixo
+    //events.split(' ') -> faz a separação das strings para cada um ser um elemento
+    //forEach -> Para poder receber essa separação das string e criar um array
+    //Necessário fazer assim pois só consegue escutar um elemento por vez
+    addEventListenerAll(element, events, fn){//Recebe o elemento, o evento(click) e a função
         events.split(' ').forEach(event =>{
             element.addEventListener(event, fn, false)
         });
     }
 
+    //Apaga tudo
     clearAll(){
         this._operation = [];
     }
 
+    //Apaga a ultima entrada
     clearEntry(){
         this._operation.pop();
 
     }
 
-    addOperation(value){
+    //Pega o ultimo valor do array pelo Lenght
+    getlastOperation(){
+        return this._operation[this._operation.length - 1];
+    }
+
+    //Função para pegar o ultimo numero do array, SÓ PEGAR
+    setLastOperation(value){
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    //Verifica se é um operador se for é TRUE senão é FALSE
+    //Lembrando que os sinais strings são os mesmo que estão nos IDs dos botões
+    //Se for mais que -1 é um operador, porque no array o + é o [0] e assim por diante
+    isOperator(value){
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+    }
+
+    //Função para verificar se já passou de 2 numeros para fazer a operação requerida
+    //..(manda 2 numeros soma e mostra resultado e depois vai para o próximo)
+    pushOperation(value){
         this._operation.push(value);
+
+        if (this._operation.length > 3) {
+            this.calc();
+        }
+    }
+
+    //Faz a soma de fato
+    calc(){
+        let last = this._operation.pop();//retira o ultimo numero para ficar so com 2 para somar
+        let result = eval(this._operation.join(""));
+        //Eval faz somar independente se o sinal for uma string
+        //E usa o join para poder tirar as virgulas da separação do array dos numeros
+        this._operation = [result, last];
+    }
+
+    setLastNumberToDisplay(){
+        
+    }
+
+    //Adiciona o operador no final da expressão, ou seja separa a expressão para 
+    //..salvar os numeros e depois fazer a conta, para não ficar somando a cada apertar
+    //..tipo 102 e não 12
+    //Se quando enquanto a tecla for apertada não for um numero faça
+    addOperation(value){
+        console.log("aa", value, this.getlastOperation)
+        if (isNaN(this.getlastOperation())){
+            //string
+            if (this.isOperator(value)) {
+                //trocar o operador
+                this.setLastOperation(value);
+
+            }else if(isNaN(value)){
+
+                console.log(value)
+            } else{
+                this.pushOperation.push(value);
+            }
+
+        }else{//Esta convertendo para string tudo e jogando no push os numeros, para não somar 102 e não 12
+            if (this.isOperator(value)){
+                this.pushOperation.push(value);
+            }else {
+                let newValue = this.getlastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+            //atualizar display
+            this.setLastNumberToDisplay();
+            }
+            
+            
+        }
+
+
+        
 
         console.log(this._operation);
     }
 
+    //Verifica se tem erro na calculadora
     setError(){
         this.displayCalc = "Error";
     }
 
+    //Chama as funções para cada botão que for pressionado
     execBtn(value){
 
         switch(value){
@@ -56,28 +137,33 @@ class CalcController{
                 this.clearEntry();
             break;
 
+            //Se apertar o botão Somar que tem o texto soma como ID, alterar para mostrar no visor o sinal de + como uma string
             case "soma":
-                this.somar();
+                this.addOperation('+');
             break;
 
             case "subtracao":
-                this.somar();
+                this.addOperation('-');
             break;
 
             case "divisao":
-                this.somar();
+                this.addOperation('/');
             break;
 
             case "multiplicacao":
-                this.somar();
+                this.addOperation('*');
             break;
 
             case "porcento":
-                this.somar();
+                this.addOperation('%');
             break;
 
             case "igual":
-                this.somar();
+                //this.addOperation('=');
+            break;
+
+            case "ponto":
+                this.addOperation('.');
             break;
 
             case "0":
@@ -92,7 +178,8 @@ class CalcController{
             case "9":
                 this.addOperation(parseInt(value));
             //adiciona o value que recebeu do botão e converte para inteiro
-            //Precisa ser feito desta forma pois o nome dos botões é uma string 
+            //Precisa ser feito desta forma pois o nome dos botões é uma string
+            //E não teria como somar depois.
             break;
 
             default:
@@ -111,8 +198,9 @@ class CalcController{
             this.addEventListenerAll(btn, 'click drag', e => {
                 //console.log(btn.className.baseVal.replace("btn-",""));
                 let textBtn = btn.className.baseVal.replace("btn-","");
+                //Retira o btn- da formatação para poder trazer apenas o 9 ao inves de btn-9
                 this.execBtn(textBtn);
-
+                //chama a função do execBtn que chama as funções de fato.
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
