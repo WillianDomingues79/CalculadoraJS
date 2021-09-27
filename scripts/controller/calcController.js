9f806881ea821d4e63116e121408cad4ee746d04
WillianDomingues79 <54421683+WillianDomingues79@users.noreply.github.com>
2021-09-27 13:28:38 -0300

    Adicionado função de teclado

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -12,6 +12,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
         //this.addEventListenerAll();
     }
 
@@ -32,6 +33,49 @@ class CalcController {
     addEventListenerAll(element, events, fn) {//Recebe o elemento, o evento(click) e a função
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false)
+        });
+    }
+    
+    initKeyboard(){
+        document.addEventListener('keyup', e=>{
+            console.log(e.key);
+
+            switch(e.key){
+                case "Escape":
+                    this.clearAll();
+                break;
+                case "Backspace":
+                    this.clearEntry();
+                break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "%":
+                    this.addOperation(e.key);
+                break;
+                case "Enter":
+                case "=":
+                    this.calc();
+                break;
+                case ".":
+                case ",":
+                    this.addDot();
+                break;
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    this.addOperation(e.key);
+                break;
+
+            }
         });
     }
 
