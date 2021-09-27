de5b1bc418e640102c3ca2f52401014de926d5df
WillianDomingues79 <54421683+WillianDomingues79@users.noreply.github.com>
2021-09-27 12:39:51 -0300

    Adicionado a função de apertar varias vezes o igual e somar os numeros

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,9 @@
 class CalcController {
 
     constructor() {//ATRIBUTOS
+        this._lastOperator = '';
+        this._lastNumber = '';
+
         this._operation = [];
         this.locale = 'pt-BR'//Criado pois irá usar varias vezes
         this._displayCalcEl = document.querySelector("#display")
@@ -74,17 +77,36 @@ class CalcController {
         }
     }
 
+    getResult(){
+        return eval(this._operation.join(""));
+        //Eval faz somar independente se o sinal for uma string
+            //E usa o join para poder tirar as virgulas da separação do array dos numeros
+    }
+
     //Faz a soma de fato
     calc() {
         let last = '';
 
-        if(this._operation.length > 3){
-            last = this._operation.pop();//retira o ultimo numero para ficar so com 2 para somar
+        this._lastOperator = this.getLastItem();
+
+        if (this._operation.length < 3){
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
         }
 
-        let result = eval(this._operation.join(""));
-            //Eval faz somar independente se o sinal for uma string
-            //E usa o join para poder tirar as virgulas da separação do array dos numeros
+        if(this._operation.length > 3){
+            last = this._operation.pop();//retira o ultimo numero para ficar so com 2 para somar
+            this._lastNumber = this.getResult();
+
+        }else if(this._operation.length == 3){
+            //last = this._operation.pop();//retira o ultimo numero para ficar so com 2 para somar
+            this._lastNumber = this.getLastItem(false);
+        }
+
+        console.log('_lastOperator', this._lastOperator);
+        console.log('_lastNumber', this._lastNumber);
+
+        let result = this.getResult();
         
         if (last == '%'){
             result /= 100;
@@ -100,15 +122,29 @@ class CalcController {
         this.setLastNumberToDisplay();
     }
 
-    setLastNumberToDisplay() {
-        let lastNumber;
+    getLastItem(isOperator = true){
+        let lastItem;
 
         for (let i = this._operation.length - 1; i >= 0; i--) {
-            if (!this.isOperator(this._operation[i])) {
-                lastNumber = this._operation[i];
-                break;
+
+            
+                if (this.isOperator(this._operation[i]) == isOperator) {
+                    lastItem = this._operation[i];
+                    break;
+                }
+
+            
+            if(!lastItem){
+                lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
             }
+            
         }
+        return lastItem;
+    }
+
+    setLastNumberToDisplay() {
+        let lastNumber = this.getLastItem(false);
+
 
         if(!lastNumber) lastNumber = 0;
 
